023766d0b203390dd334647b3276342c1197d94a
Alex Mauricio Castillo <alecasti@nttdata.com>
2025-09-03 19:48:49 -0500

    inicial

---
+++ src/main/java/com/lite/libreria/ClaseBean.java
@@ -0,0 +1,12 @@
+package com.lite.libreria;
+
+public class ClaseBean {
+    
+    public String saludar() {
+        return "Hola desde ClaseBean";
+    }
+
+    public String despedir() {
+        return "Adiós desde ClaseBean";
+    }
+}
